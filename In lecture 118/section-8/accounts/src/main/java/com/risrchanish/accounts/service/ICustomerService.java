776ea7cd45767a49853aae3ec60bf0d430f29320
risrchanish <risrchanish@gmail.com>
2025-10-19 06:49:41 +0530

    Delete In lecture 118/section-8 directory

--- In lecture 118/section-8/accounts/src/main/java/com/risrchanish/accounts/service/ICustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.risrchanish.accounts.service;
-
-import com.risrchanish.accounts.dto.CustomerDetailsDto;
-
-public interface ICustomerService {
-
-	CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
-}
